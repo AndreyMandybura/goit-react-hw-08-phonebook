85f875a3a133a904c374723e452ddac3a55c315b
AndreyMandybura <80692584+AndreyMandybura@users.noreply.github.com>
2022-01-08 15:06:39 +0200

    Приватные/публичные маршруты+ошибки при логинизации

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-// import ContactForm from './components/ContactForm/ContactForm';
-// import Filter from './components/Filter/Filter';
-// import ContactList from './components/ContactList/ContactList';
-
-import { lazy, Suspense } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
 import Container from './components/Container/Container';
 import AppBar from './components/AppBar';
 import Loader from './components/Loader/Loader';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+import PublicRoute from './components/PublicRoute/PublicRoute';
+import { authOperations, authSelectors } from './redux/auth';
 
 const HomeView = lazy(() => import('./views/HomeView'));
 const RegisterView = lazy(() => import('./views/RegisterView'));
@@ -15,18 +15,59 @@ const ContactsView = lazy(() => import('./views/ContactsView'));
 const NotFoundView = lazy(() => import('./views/NotFoundView'));
 
 export default function App() {
+  const dispatch = useDispatch();
+  const isFetchingCurrentUser = useSelector(authSelectors.getIsFetchingCurrent);
+
+  useEffect(() => {
+    dispatch(authOperations.fetchCurrentUser());
+  }, [dispatch]);
+
   return (
     <Container>
-      <AppBar />
-      <Suspense fallback={<Loader />}>
-        <Routes>
-          <Route path="/*" element={<HomeView />} />
-          <Route path="/register" element={<RegisterView />} />
-          <Route path="/login" element={<LoginView />} />
-          <Route path="/contacts" element={<ContactsView />} />
-          <Route path="/*" element={<NotFoundView />} />
-        </Routes>
-      </Suspense>
+      {isFetchingCurrentUser ? (
+        <h1>Loading...</h1>
+      ) : (
+        <>
+          <AppBar />
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route
+                path="/*"
+                element={
+                  <PublicRoute>
+                    <HomeView />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="/register"
+                element={
+                  <PublicRoute restricted>
+                    <RegisterView />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  <PublicRoute redirectTo="/contacts" restricted>
+                    <LoginView />
+                  </PublicRoute>
+                }
+              />
+              <Route
+                path="/contacts"
+                element={
+                  <PrivateRoute redirectTo="/login">
+                    <ContactsView />
+                  </PrivateRoute>
+                }
+              />
+              <Route path="/*" element={<NotFoundView />} />
+            </Routes>
+          </Suspense>
+        </>
+      )}
     </Container>
   );
 }
